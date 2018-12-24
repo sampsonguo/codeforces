cd294d00f1a5d30d45f7d7261ecc1c76deb0bfb1
root <root@bp-dm-recommend-pushsystem001.bjthq.vivo.lan>
2018-12-24 20:13:00 +0800

    update

--- 528/C.py
+++ 528/C.py
@@ -4,12 +4,28 @@ ax, ay = map(int, raw_input().split())
 bx, by = map(int, raw_input().split())
 cx, cy = map(int, raw_input().split())
 
-min_x = min(ax, bx, cx)
-max_x = max(ax, bx, cx)
+if ax > bx:
+    ax, bx = bx, ax
+    ay, by = by, ay
+if bx > cx:
+    bx, cx = cx, bx
+    by, cy = cy, by
+if ax > bx:
+    ax, bx = bx, ax
+    ay, by = by, ay
 
-min_y = min(ay, by, cy)
-max_y = max(ay, by, cy)
+ret = []
+for i in range(min(ay, by, cy), max(ay, by, cy)+1):
+    ret.append([bx, i])
 
+for i in range(ax, bx):
+    ret.append([i, ay])
 
+for i in range(bx+1, cx+1):
+    ret.append([i, cy])
+
+print len(ret)
+for i in range(len(ret)):
+    print("%d %d"%(ret[i][0], ret[i][1]))
 
 
